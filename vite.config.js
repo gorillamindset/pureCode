e73cbe249ab6bf51b54c5ea0a3c4191d33c23d3f
Abhishek <138226552+gorillamindset@users.noreply.github.com>
2024-03-29 06:55:19 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/yum_r/",
+  plugins: [react()],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
